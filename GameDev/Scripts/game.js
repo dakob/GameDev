a0a293661a3f78771c192ff9ac4f0297d4ea4e47
dakob <damian.kobak@gmail.com>
2014-09-26 07:45:58 +0200

    Fixed game.js
    Main fix for game.

--- GameDev/Scripts/game.js
+++ GameDev/Scripts/game.js
@@ -1,6 +1,6 @@
 ﻿var clamp = function (x, min, max) {
-    return x < min? min: x > max ? max: x;
-}
+    return x < min ? min : (x > max ? max : x);
+};
 
 var Q = Quintus()
     .include("Sprites, Anim, Input, Touch, Scenes, UI")
@@ -8,14 +8,15 @@ var Q = Quintus()
     .touch();
 
 Q.input.touchControls({
-    controls: [
-    ['left', '<'],
-    ['right', '>'],
-[],
-[],
-[],
-[],
-['fire', 'e'],
+    controls:
+    [
+        ['left', '<'],
+        ['right', '>'],
+        [],
+        [],
+        [],
+        [],
+        ['fire', 'a']
     ]
 });
 
@@ -35,7 +36,7 @@ Q.Sprite.extend("Player", {
         this.add("animation");
         this.play("default");
         this.add("Gun");
-        this.on("hit", function (col) {
+        this.on("hit", function(col) {
             if (col.obj.isA("Shot") && ((col.obj.p.type & Q.SPRITE_ENEMY) == Q.SPRITE_ENEMY)) {
                 this.destroy();
                 col.obj.destroy();
@@ -48,41 +49,43 @@ Q.Sprite.extend("Player", {
             this.p.x -= this.p.speed;
         if (Q.inputs['right'])
             this.p.x += this.p.speed;
+
         this.p.x = clamp(this.p.x, 0 + (this.p.w / 2), Q.el.width - (this.p.w / 2));
         this.stage.collide(this);
     }
 });
 
-Q.Sprite.extend("Alien", {
-    init: function (p) {
-        this._super(p, {
-            sprite: "alien",
-            sheet: "alien",
-            x: Q.el.width / 2,
-            speed: 200
-        });
+Q.Sprite.extend("Alien", {    
+   init: function(p) {
+       this._super(p, {           
+           sprite: "alien",
+           sheet: "alien",
+           x: Q.el.width / 2,
+           speed: 200
+       });
 
-        this.p.y = this.p.h;
-        this.add("animation");
-        this.play("default");
-        this.add("BasicAI");
-        this.on("hit", function (col) {
-            if (col.obj.isA("Shot") && ((col.obj.p.type & Q.SPRITE_FRIENDLY) == Q.SPRITE_FRIENDLY)) {
-                this.destroy();
-                col.obj.destroy();
+       this.p.y = this.p.h;
+       this.add("animation");
+       this.play("default");
+       this.add("BasicAI");
+       this.on("hit", function(col)
+       {
+           if (col.obj.isA("Shot") && ((col.obj.p.type & Q.SPRITE_FRIENDLY) == Q.SPRITE_FRIENDLY)) {
+               this.destroy();
+               col.obj.destroy();
                Q.stageScene("endGame", 1, { label: "You Won!" });
-            }
-        });
-    },
-    step: function (dt) {
-        this.stage.collide(this);
-    }
+           }
+       });
+   },
+   step: function(dt) {
+       this.stage.collide(this);
+   }
 });
 
 Q.Sprite.extend("Shot", {
     init: function (p) {
         this._super(p, {
-            sprite: "shot", 
+            sprite: "shot",
             sheet: "shot",
             speed: 200
         });
@@ -92,46 +95,42 @@ Q.Sprite.extend("Shot", {
     },
     step: function (dt) {
         this.p.y -= this.p.speed * dt;
-
-        if (this.p.y > Q.el.height || this.p.y < 0){
+        
+        if (this.p.y > Q.el.height || this.p.y < 0) {
             this.destroy();
-		}
+        }
     }
 });
 
-Q.component("BasicAI", {
-
-    added: function () {
-        this.entity.changeDirection();
+Q.component("BasicAI", {    
+    added: function() {
+        this.entity.changeDirections();
         this.entity.on("step", "move");
         this.entity.on("step", "tryToFire");
         this.entity.add("Gun");
     },
-
     extend: {
-        changeDirection: function () {
+        changeDirections: function() {
             var entity = this;
             var numberOfSeconds = Math.floor((Math.random() * 5) + 1);
-            setTimeout(function () {
+            setTimeout(function() {
                 entity.p.speed = -entity.p.speed;
-                entity.changeDirection();
+                entity.changeDirections();
             }, numberOfSeconds * 1000);
         },
-
-        move: function (dt) {
+        move: function(dt) {
             var entity = this;
             entity.p.x -= entity.p.speed * dt;
-            if (entity.p.x > Q.el.width - (entity.p.w / 2) ||
-                entity.p.x < 0 + (entity.p.w / 2)) {
+            if (entity.p.x > Q.el.width - (entity.p.w / 2) || entity.p.x < 0 + (entity.p.w / 2)) {
                 entity.p.speed = -entity.p.speed;
             }
         },
-        tryToFire: function () {
+        
+        tryToFire: function() {
             var entity = this;
             var player = Q("Player").first();
             if (!player)
                 return;
-            
             if (player.p.x + player.p.w > entity.p.x && player.p.x - player.p.w < entity.p.x) {
                 this.fire(Q.SPRITE_ENEMY);
             }
@@ -149,11 +148,12 @@ Q.component("Gun", {
     extend: {
         handleFiring: function (dt) {
             var entity = this;
-
-            for (var i = entity.p.shots.length - 1; i >= 0 ; i--) {
-                if (entity.p.shots[i].isDestroyed)
-                    entity.p.shots.splice(i, 1);
-             }
+            
+            for (var i = entity.p.shots.length - 1; i >= 0; i--) {
+                if (entity.p.shots[i].isDestroyed) {
+                    entity.p.shots.splice(i, 1);                 
+                }
+            }
 
             if (Q.inputs['fire'] && entity.p.type == Q.SPRITE_FRIENDLY) {
                 entity.fire(Q.SPRITE_FRIENDLY);
@@ -162,11 +162,11 @@ Q.component("Gun", {
 
         fire: function (type) {
             var entity = this;
-           
+
             if (!entity.p.canFire)
                 return;
 
-            var shot; 
+            var shot;
             if (type == Q.SPRITE_FRIENDLY) {
                 shot = Q.stage().insert(new Q.Shot({ x: entity.p.x, y: entity.p.y - 50, speed: 200, type: Q.SPRITE_DEFAULT | Q.SPRITE_FRIENDLY }));
             } else {
@@ -174,9 +174,10 @@ Q.component("Gun", {
             }
             entity.p.shots.push(shot);
             entity.p.canFire = false;
-            setTimeout(function () {
+            setTimeout(function() {
                 entity.p.canFire = true;
             }, 500);
+
         }
     }
 });
@@ -186,18 +187,34 @@ Q.scene("mainLevel", function (stage) {
     stage.insert(new Q.Sprite({ asset: "spacebckg.png", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE }));
     stage.insert(new Q.Player());
     stage.insert(new Q.Alien());
+});
 
+Q.scene("endGame", function(stage) {
+    var container = stage.insert(new Q.UI.Container({        
+        x: Q.width / 2, y: Q.height /2, fill: "#FFFFFF"
+    }));
+
+    var button = container.insert(new Q.UI.Button({        
+        x: 0, y: 0, fill: "#CCCCCC", label: "Play Again"
+    }));
+
+    container.insert(new Q.UI.Text({        
+       x: 10, y: -10 - button.p.h, label: stage.options.label
+    }));
+    button.on("click", function() {
+        Q.clearStages();
+        Q.stageScene("mainLevel");
     });
+    container.fit(20);
+});
 
-Q.load(["spacebckg.png", "spaceship2.png", "shot.png", "alien.png", "player.json", "shot.json",
-    "alien.json"], function () {
-    Q.compileSheets("spaceship2.png", "player.json");
-    Q.compileSheets("shot.png", "shot.json");
-    Q.compileSheets("alien.png", "alien.json");
-    Q.animations("player", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
-    Q.animations("shot", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
-    Q.animations("alien", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
-
-    Q.stageScene("mainLevel");
-
+Q.load(["spacebckg.png", "spaceship2.png", "shot.png", "alien.png",
+    "player.json", "shot.json", "alien.json"], function () {
+        Q.compileSheets("spaceship2.png", "player.json");
+        Q.compileSheets("shot.png", "shot.json");
+        Q.compileSheets("alien.png", "alien.json");
+        Q.animations("player", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
+        Q.animations("shot", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
+        Q.animations("alien", { default: { frames: [0, 1, 2, 3], rate: 1 / 4 } });
+        Q.stageScene("mainLevel");
     });
